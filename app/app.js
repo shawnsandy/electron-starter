fb53130397f1a18262f45f698b3bf24bd73b66e9
Shawn Sandy <shawnsandy04@gmail.com>
2015-08-16 15:26:11 -0400

    Window Demo
    Added sample that launches new window
    Toggle the launch window button to open / close using sample window
    set frame to false

--- app/app.js
+++ app/app.js
@@ -8,6 +8,8 @@ import { greet } from './hello_world/hello_world';
 
 // Node modules are required the same way as always.
 var os = require('os');
+var ipc = require('ipc');
+
 
 // window.env contains data from config/env_XXX.json file.
 var envName = window.env.name;
@@ -15,3 +17,11 @@ var envName = window.env.name;
 document.getElementById('greet').innerHTML = greet();
 document.getElementById('platform-info').innerHTML = os.platform();
 document.getElementById('env-name').innerHTML = envName;
+
+var launchBtn = document.getElementById('launch-btn');
+
+launchBtn.addEventListener('click', function(e){
+  //e.preventDefault();
+  //console.log('launchBtn clicked');
+  ipc.send('toggle-window');
+});
