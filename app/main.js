c1a9569357348917474b18c7951f53bcc5199ec9
Shawn Sandy <shawnsandy04@gmail.com>
2015-08-17 07:35:06 -0400

    Minor Fixes
    removed required app.js from app-window.html web-page.html
    re-enabled chrome dev tools

--- app/main.js
+++ app/main.js
@@ -34,7 +34,7 @@ app.on('ready', function() {
 
     if (env.name === 'development') {
         devHelper.setDevMenu();
-        //mainWindow.openDevTools();
+        mainWindow.openDevTools();
     }
 
     mainWindow.on('close', function() {
