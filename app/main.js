838723042950ca17c457f77e3be4adffb2461042
Shawn Sandy <shawnsandy04@gmail.com>
2015-08-20 14:04:51 -0400

    removed sample windows
    reverting to barebones min.’s with, only added cut and paste

--- app/main.js
+++ app/main.js
@@ -41,46 +41,6 @@ app.on('ready', function() {
         mainWindowState.saveState(mainWindow);
     });
 
-    //launch new window
-    var appWindow = new BrowserWindow({
-        width: 400,
-        height: 400,
-        show: false,
-        x: 0,
-        y: 0,
-        frame: false
-    });
-    appWindow.loadUrl('file://' + __dirname + '/ui/app-win.html');
-
-    appWindow.on('closed', function() {
-        appWindow = null;
-    });
-
-    ipc.on('toggle-window', function() {
-        if (appWindow.isVisible())
-            appWindow.hide();
-        else
-            appWindow.show();
-    })
-
-    // launch a webview
-    var webPage = new BrowserWindow({
-      width: 1024,
-      height: 768,
-      frame: false,
-      show: false,
-      'skip-taskbar': true
-    });
-
-    webPage.loadUrl('file://' + __dirname + '/ui/web-page.html');
-
-    ipc.on('webview', function(){
-      if(webPage.isVisible())
-      webPage.hide();
-      else
-      webPage.show();
-    });
-
     // Create the Application's main menu
     var template = [{
         label: "Application",
