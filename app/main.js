b229a3c6501e79ef347fd86c2b3b06ef60c2f530
Shawn Sandy <shawnsandy04@gmail.com>
2015-08-25 22:00:19 -0400

    restored to boilerplate version

--- app/main.js
+++ app/main.js
@@ -1,41 +1,26 @@
 'use strict';
 
 var app = require('app');
-var Menu = require('menu');
-var ipc = require('ipc');
 var BrowserWindow = require('browser-window');
 var env = require('./vendor/electron_boilerplate/env_config');
-//var devHelper = require('./vendor/electron_boilerplate/dev_helper');
-var mainMenu = require('./js/main-menu');
+var devHelper = require('./vendor/electron_boilerplate/dev_helper');
 var windowStateKeeper = require('./vendor/electron_boilerplate/window_state');
-//crash reporter
-var crashReporter = require('crash-reporter');
-
-
-crashReporter.start({
-    productName: 'YourName',
-    companyName: 'YourCompany',
-    submitUrl: 'https://your-domain.com/url-to-submit',
-    autoSubmit: true
-});
 
 var mainWindow;
 
 // Preserver of the window size and position between app launches.
 var mainWindowState = windowStateKeeper('main', {
-    width: 400,
-    height: 300
+    width: 1000,
+    height: 600
 });
 
-app.on('ready', function() {
+app.on('ready', function () {
 
     mainWindow = new BrowserWindow({
         x: mainWindowState.x,
         y: mainWindowState.y,
         width: mainWindowState.width,
-        height: mainWindowState.height,
-        icon: __dirname + '/images/icons/neutron.png',
-        title: 'Deskit'
+        height: mainWindowState.height
     });
 
     if (mainWindowState.isMaximized) {
@@ -45,46 +30,15 @@ app.on('ready', function() {
     mainWindow.loadUrl('file://' + __dirname + '/app.html');
 
     if (env.name === 'development') {
-       // devHelper.setDevMenu();
+        devHelper.setDevMenu();
         mainWindow.openDevTools();
     }
 
-    mainWindow.on('close', function() {
+    mainWindow.on('close', function () {
         mainWindowState.saveState(mainWindow);
     });
-
-   mainMenu.appMenu();
-
 });
 
-app.on('window-all-closed', function() {
+app.on('window-all-closed', function () {
     app.quit();
-});
-
-
-var settingsWindow = null;
-
-ipc.on('open-settings-window', function () {
-    if (settingsWindow) {
-        return;
-    }
-
-    settingsWindow = new BrowserWindow({
-        frame: false,
-        height: 200,
-        resizable: false,
-        width: 200
-    });
-
-    settingsWindow.loadUrl('file://' + __dirname + '/app/settings.html');
-
-    settingsWindow.on('closed', function () {
-        settingsWindow = null;
-    });
-});
-
-ipc.on('close-settings-window', function () {
-    if (settingsWindow) {
-        settingsWindow.close();
-    }
 });
